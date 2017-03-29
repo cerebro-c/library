07d503dd5a16f295d9204c283d831806b1f2c05f
cerebro-c <manueleuclides22@gmail.com>
2017-03-29 17:26:30 +0100

    Create view new book, alter route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #resources :books
+  resources :book, only: [:new]
   root to: 'book#list'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
