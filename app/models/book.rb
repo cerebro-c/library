685e194d65cadfd2544126af77c8315405458f68
cerebro-c <manueleuclides22@gmail.com>
2017-04-03 14:02:22 +0100

    Save book on database - List book from database

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ActiveRecord::Base
     belongs_to :subject
 
     validates_presence_of :title
-    validates_numericality_of :proce, :message => "Error Messagge"
+    validates_numericality_of :price, :message => "Error Messagge"
 end
